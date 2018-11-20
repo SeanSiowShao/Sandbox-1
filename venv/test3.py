9c961dc04689c8f4e7af1ae942b184f3c17e53e2
Raunak Dutta <45163382+Raunak-Dutta@users.noreply.github.com>
2018-11-20 14:20:21 +0800

    Test 1

--- venv/test3.py
+++ venv/test3.py
@@ -6,22 +6,36 @@ def main():
     NUMBERS = "1234567890"
     CONSONANTS = "bcdfghjklmnpqrstvwxyzaeiou"
     SPCGAR = "!@#$%^&*()_+"
+    VOWELS = "aeiou"
     count = int(input("please the length of password"))
     reps = 0
 
-    word_format = "cnscsnssnsccnssncssc"
+    word_format = str(input("Please enter format"))
     word = ""
-    for kind in word_format:
-        if(count >= reps):
-            if kind == "c":
-                word += random.choice(CONSONANTS)
-            elif kind == "n":
-                word += random.choice(NUMBERS)
-            elif kind == "s":
-                word += random.choice(SPCGAR)
-            else:
-                word += random.choice(VOWELS)
-            reps= reps + 1
-
+    if format_test(word_format) == True:
+        for kind in word_format:
+            if(count >= reps):
+                if kind == "c":
+                    word += random.choice(CONSONANTS)
+                elif kind == "n":
+                    word += random.choice(NUMBERS)
+                elif kind == "s":
+                    word += random.choice(SPCGAR)
+                else:
+                    word += random.choice(VOWELS)
+                reps= reps + 1
+    else:
+        print("please enter valid string and re-run the program")
     print(word)
+
+def format_test(str_test):
+    trig = 0
+    print(str_test)
+    for w in str_test:
+        if (w != "c" and w!="v" and w!="n" and w != "s"):
+            print("*")
+            return False
+            trig = 1
+    if trig == 0:
+        return True
 main()
