d85b59c888cee43fcf4e6676dd9ed4216ee3f901
Raunak Dutta <45163382+Raunak-Dutta@users.noreply.github.com>
2018-11-20 13:34:20 +0800

    Initial commit

---
+++ Sandbox/test1.py
@@ -0,0 +1,11 @@
+def main():
+    inp = str(input("enter string"))
+    pos = 0
+    ns = ""
+    if(len(inp)>0):
+        for w in inp:
+            pos = pos + 1
+            if(pos % 2 ==1):
+                ns = ns + w
+        print(ns)
+main()
